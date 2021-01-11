37b01a597389544a30fb97c5c18aafd8ca7a5266
artemian4ik <nyashev228@gmail.com>
2021-01-11 21:07:33 +0300

    first commit

---
+++ src/ggggg/main.java
@@ -0,0 +1,23 @@
+package ggggg;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public class Ingener implements interface_employee {
+		
+	}
+	
+	public class Manager implements interface_employee {
+		
+	}
+	
+	public interface interface_employee {
+		
+	}
+
+}
+

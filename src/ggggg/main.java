c7c5bc27e13d84aee9d854ed1a59a9c8b5a167f4
artemian4ik <nyashev228@gmail.com>
2021-01-11 21:19:28 +0300

    class manager ready

--- src/ggggg/main.java
+++ src/ggggg/main.java
@@ -12,10 +12,43 @@ public class main {
 	}
 	
 	public class Manager implements interface_employee {
+		int id;
+		String fio;
+		String data;
+		String position = "Менеджер";
+		
+		public void SetID(int id) {
+			this.id = id;
+		}
+		public int GetID() {
+			return id;
+		}
+		public void SetFIO(String fio) {
+			this.fio = fio;
+		}
+		public String GetFIO() {
+			return fio;
+		}
+		
+		public void SetDataStartWork(String data) {
+			this.data = data;
+		}
+		public String GetDataStartWork() {
+			return data;
+		}
 		
 	}
 	
+	/* Interface employee */
 	public interface interface_employee {
+		void SetID(int id);
+		int GetID();
+		
+		void SetFIO(String fio);
+		String GetFIO();
+		
+		void SetDataStartWork(String data);
+		String GetDataStartWork();
 		
 	}
 

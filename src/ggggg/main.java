a5679d929e4db198106460054bf73fb631d74c79
artemian4ik <nyashev228@gmail.com>
2021-01-11 21:21:58 +0300

    class ingener ready

--- src/ggggg/main.java
+++ src/ggggg/main.java
@@ -8,6 +8,34 @@ public class main {
 	}
 	
 	public class Ingener implements interface_employee {
+		int id;
+		String fio;
+		String data;
+		String position = "Инженер";
+		
+		public void SetID(int id) {
+			this.id = id;
+		}
+		public int GetID() {
+			return id;
+		}
+		public void SetFIO(String fio) {
+			this.fio = fio;
+		}
+		public String GetFIO() {
+			return fio;
+		}
+		
+		public void SetDataStartWork(String data) {
+			this.data = data;
+		}
+		public String GetDataStartWork() {
+			return data;
+		}
+		
+		public String GetPosition() {
+			return position;
+		}
 		
 	}
 	
@@ -35,6 +63,10 @@ public class main {
 		}
 		public String GetDataStartWork() {
 			return data;
+		}
+		
+		public String GetPosition() {
+			return position;
 		}
 		
 	}
